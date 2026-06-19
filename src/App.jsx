bcdf0764255a736a04a5551724fb49f6652a18a3
Joaquín nicolás Ibañez chicchi <ibanezjoaquin024@gmail.com>
2026-06-19 19:58:10 -0300

    se agrego estilos al header y footer

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+
+const App= ()=>{
+    return(
+        <div>
+           <Header/>
+           <Footer/>
+        </div>        
+
+
+    );
+}
+export default App;
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,10 @@
+import '../../App.css';
+const Footer =() =>{
+
+    return(
+        <footer className='text-white text-center py-3 mt-5'>
+            <p>Gestion de clientes 2026</p>
+        </footer>
+    );
+}
+export default Footer;
