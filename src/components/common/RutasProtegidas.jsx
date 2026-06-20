ae486aa2a8d50a8b683d5e6406b308053dbf76ed
nicoloop745 <206437489+Nicolop745@users.noreply.github.com>
2026-06-20 18:57:32 -0300

    añadiendo el archivo RutaProtegidas

---
+++ src/components/common/RutasProtegidas.jsx
@@ -0,0 +1,14 @@
+import { useAdmin } from "../../context/AdminContext";
+import { Navigate } from "react-router-dom";
+
+const RutaProtegida = ({ children }) => {
+  const { admin } = useAdmin();
+
+  if (!admin) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+export default RutaProtegida;
